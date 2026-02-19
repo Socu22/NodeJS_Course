14684d59ea16bfee8a9efb461624e326e4716116
LucasPK <149070654+Socu22@users.noreply.github.com>
2026-02-19 09:26:26 +0100

    class 03 movies gennemgået i klasses

--- 03_movies/app.js
+++ 03_movies/app.js
@@ -12,8 +12,8 @@ let movies = [
   {id: 2, title: "ironman"},
 
 ]
-let nextID = 2;  
-
+let nextID = 3;  
+let currentId= 2;
 app.get("/movies",(req,res) => {
     console.log(movies);
     res.send({data: movies});
@@ -26,19 +26,30 @@ app.get("/movies/:id", (req,res) => {
         res.send({errorMessage: `no movie found by id: ${req.params.id}`}) // dont have an res with no return that can go to one with res
     }
     else {
-        res.send(foundMovie)
+        res.send({data: foundMovie});
     }
 });
 
+//undefined.id = 5
+
 // post 
 app.post("/movies", (req,res) => {
-    const newMovie = {
-        id: nextID++,
-        title: req.body.title
+    //const newMovie = {        id: nextID++,        title: req.body.title    }
+    //movies.push(newMovie);
+    //console.log(movies);
+    //res.send({data : newMovie});
+    if (!req.body){
+        return res.status(400).send({errorMessage: 'No Json body provided.'})
     }
-    movies.push(newMovie)
-    console.log(movies);
-    res.send({data : newMovie})
+    const providedMovie = req.body;
+    //providedMovie.id =  currentId++; // increments after it should be used. Prefix notation. 
+    //providedMovie.id =  ++currentId;
+    providedMovie.id = nextID++;
+++
+    movies.push(providedMovie);
+    res.send({data: providedMovie})
+
+
 
 });
 
@@ -53,28 +64,47 @@ app.put("/movies/:id", (req, res) => {
         res.send({ errorMessage: `no movie found by id: ${req.params.id}` });
     } else {
         foundMovie.title = req.body.title;
-        res.send(foundMovie);
+        res.send({data: foundMovie});
     }
 });
 
+// patch. Many places are not supported. therefore most use put to delete the whole thing and replace it with body of request.
+app.patch("/movies/:id", (req,res) => {
+    const providedMovieId = Number(req.params.id);
+    const foundMovieIndex = movies.findIndex( (movie) => movie.id === providedMovieId);  
+
+
+    if (!foundMovie) {
+        return res.status(404).send({ errorMessage: `no movie found by id: ${req.params.id}` });
+    } 
+    const foundMovie = movies[foundMovieIndex]
+    const providedMovie = req.body;
+
+    const movieToCreate = {...foundMovie, ...providedMovie, id: providedMovieId}; // provided movie will overwrite attributes key value pairs to the last mentioned one. you could overwrite id so you will make the last mention of the id to what i originaly was. Spread operator ...
+
+    movies[foundMovieIndex] = movieToCreate;
+
+    res.send({data: movieToCreate})
+
+})
 
 // delete
 app.delete("/movies/:id", (req, res) => {
     const providedMoviesId = Number(req.params.id);
-    const movieIndex = movies.findIndex( (movie) => movie.id === providedMoviesId);
-    if(!movieIndex === -1) {
-        res.send({errorMessage: `no movie found by id: ${req.params.id}`} );
+    const foundMovieIndex = movies.findIndex( (movie) => movie.id === providedMoviesId); // returns -1 if it does not exist 
+    if(!foundMovieIndex === -1) {
+        return  res.status(404).send({errorMessage: `no movie found by id: ${req.params.id}`})
     }
-    else {
-        const deletedMovie = movies.splice(movieIndex, 1)[0];
-        res.send(deletedMovie);
-    }
+    
+    movies.splice(foundMovieIndex,1)
+    res.status(204).send();
+    
 
 });
 /*
 status codes:
 2xx: Succes
-3xx: Redirection
+3xx: Redirection 
 4xx: Client error 
 5xx: Server error 
 
