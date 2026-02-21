0511cb9dd62d430f54f2424a63a6309206dca444
LucasPK <149070654+Socu22@users.noreply.github.com>
2026-02-21 12:55:52 +0100

    TimeInsultGame version 1 is out with insults as you calculate when the hardcoded 20 secs is over

--- 04_Date/app.js
+++ 04_Date/app.js
@@ -1,8 +1,6 @@
-const express = require('express');
+const express = require("express");
 
-const app = express(); 
-
-
+const app = express();
 
 /*
 console.log(new Date()); // UTC Date + Time  
@@ -10,48 +8,57 @@ console.log(Date()); // Local Time
 console.log(Date.now()); // Unix Epoch Time (Miliseconds since Jan. 1st 1970)
 */
 
-// array of months is saved for later. 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// array of months is saved for later.
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 const days = [
-    "Sunday",
-    "Monday",
+  "Sunday",
+  "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday"
+  "Saturday",
 ];
 
-app.get("/months/v1", (req,res) => {
-    const currentMonth = new Date().getMonth();
-    
-    res.send({data: months[currentMonth]})
-    
+app.get("/months/v1", (req, res) => {
+  const currentMonth = new Date().getMonth();
 
-
+  res.send({ data: months[currentMonth] });
 });
 
-app.get("/months/v2", (req,res) => {
-    const currentMonth = new Date().toLocaleDateString('default', {month: 'long'})
-    //const currentMonth = new Date().toLocaleDateString('en-uk', {month: 'short'})
-    console.log(currentMonth);
-    
-    res.send({data: currentMonth})
-    
+app.get("/months/v2", (req, res) => {
+  const currentMonth = new Date().toLocaleDateString("default", {
+    month: "long",
+  });
+  //const currentMonth = new Date().toLocaleDateString('en-uk', {month: 'short'})
+  console.log(currentMonth);
 
-
+  res.send({ data: currentMonth });
 });
-app.get("/days/v1", (req,res) => {
-    const currentDay = new Date().getDay(); // this is an english week. Sunday is the first day of the week
+app.get("/days/v1", (req, res) => {
+  const currentDay = new Date().getDay(); // this is an english week. Sunday is the first day of the week
 
-    res.send({data: days[currentDay]});
-
+  res.send({ data: days[currentDay] });
 });
 
-app.get("/days/v2", (req,res) => {
-    const currentDay = new Date().toLocaleDateString("en-uk", {weekday: "long"}); 
-    res.send({data: currentDay});
-
+app.get("/days/v2", (req, res) => {
+  const currentDay = new Date().toLocaleDateString("en-uk", {
+    weekday: "long",
+  });
+  res.send({ data: currentDay });
 });
 /*
 console.log(new Date().getMonth())
@@ -62,24 +69,19 @@ console.log(new Date().getDate())
 console.log(Date().split(" ")[0]);
 */
 
+app.get("/timeInsultGame", (req, res) => {
+  res.sendFile(__dirname + "/public/timeInsultGame.html");
+});
 
-
-
-
-
-
-// falsy values : false, null, undefined, NaN, "", 
+// falsy values : false, null, undefined, NaN, "",
 
 app.listen(8080, (error) => {
+  if (error) {
+    // this will activate if there is anything there is not falsy values
+    console.log("error");
 
+    return;
+  }
 
-    if(error) { // this will activate if there is anything there is not falsy values
-        console.log("error");
-        
-        return;
-
-    }
-
-    console.log("Server is running on port",8080);
-    
-})
+  console.log("Server is running on port", 8080);
+});
