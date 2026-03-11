eb459830c41bb79f9a2b1204a6c512f2e44daccd
LucasPK <149070654+Socu22@users.noreply.github.com>
2026-03-11 13:39:22 +0100

    Mandatory assigmnet v2. inserted my notes + edits + html txt is usable for viewing rather than using

--- 06.Mandatory1_DocumentationServer/app.js
+++ 06.Mandatory1_DocumentationServer/app.js
@@ -11,6 +11,40 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("public/pages/index/index.html"));
 });
+/* Node.js basics */
+app.get("/nodejs-basics", (req, res) => {
+  res.sendFile(path.resolve("public/pages/nodejs-basics/nodejs-basics.html"));
+});
+
+/* JavaScript core */
+app.get("/javascript-core", (req, res) => {
+  res.sendFile(
+    path.resolve("public/pages/javascript-core/javascript-core.html"),
+  );
+});
+
+/* Running Node projects */
+app.get("/nodejs-projects", (req, res) => {
+  res.sendFile(
+    path.resolve("public/pages/nodejs-projects/nodejs-projects.html"),
+  );
+});
+
+/* Express REST + modules */
+app.get("/express-rest-modules", (req, res) => {
+  res.sendFile(
+    path.resolve("public/pages/express-rest-modules/express-rest-modules.html"),
+  );
+});
+
+/* Frontend security + tools */
+app.get("/frontend-security-tools", (req, res) => {
+  res.sendFile(
+    path.resolve(
+      "public/pages/frontend-security-tools/frontend-security-tools.html",
+    ),
+  );
+});
 
 app.get("/about", (req, res) => {
   res.sendFile(path.resolve("public/pages/about/about.html"));
